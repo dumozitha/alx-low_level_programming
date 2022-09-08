12c5fc969dc6a07cff5b0052c59e56e881ccb1d2
dumozitha <dumozitha@gmail.com>
2022-09-08 12:36:53 -0700

    size of

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -17,8 +17,8 @@ int main(void)
 
 	printf("size of a char: %ld byte(s)\n", sizeof(c));
 	printf("size of an int: %1d byte(s)\n", sizeof(i));
-	printf("size of a long int: %ld byte(s)\n", sizeof(li);
-	printf("size of a long long int: %ld byte(s)\n", sizeof(lli);
-	printf("size of a float: %ld byte(s)\n", sizeof(f);
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("size of a float: %ld byte(s)\n", sizeof(f));
 	return (0);
 }
