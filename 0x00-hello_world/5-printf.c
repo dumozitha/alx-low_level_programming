f0d21f15d7ed4f7e63a5d10a37d5d576c6cb6f79
dumozitha <dumozitha@gmail.com>
2022-09-08 11:43:59 -0700

    printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+
 /*
  * main - prints the string in the function
  *
  * Description: using the main function
  * this program prints "with proper grammar, but the outcome is a piece of art,
- * Return (0)
+ * Return: (0)
  */
 int main(void)
 {
