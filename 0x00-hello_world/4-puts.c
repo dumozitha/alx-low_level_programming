bd15dd6cffd20d7e11f7d2055b70949c699ee671
dumozitha <dumozitha@gmail.com>
2022-09-08 10:53:19 -0700

    puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - print string in put function
+ *
+ * description: using main function
+ * the program prints "Programming is like building a multilingual puzzle"
+ * rutun: 0 
+ */
+
+int main(void)
+{
+    puts("\"Programing is like building a multilingusl prussle");
+    return (0);
+}
