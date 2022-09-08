eddd1da267a24ddf876dd67a2e1f1fc7b2b7a06e
dumozitha <dumozitha@gmail.com>
2022-09-08 12:34:45 -0700

    size of

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+/**
+ * main - print the string in the print function
+ *
+ * Description: using the main function
+ * the program prints" the size of virous types of computers
+ * Return: (0)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %1d byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li);
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli);
+	printf("size of a float: %ld byte(s)\n", sizeof(f);
+	return (0);
+}
