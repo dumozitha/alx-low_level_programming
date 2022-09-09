86dc7c97b854bccc25b66a5dce101d9dce5b3512
dumozitha <dumozitha@gmail.com>
2022-09-09 04:13:33 -0700

    positve or negetive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main- program entry
+ *
+ * Description: to print n and see if value is positive or negetive
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+	
+	srand(time(0));
+	n = rand() -RAND_MAX / 2;
+
+	/*code*/
+	if (n > 0)
+		printf("%i is positve\n", n);
+	else if (n == 0)
+		printf("%i is zero\n", n);
+	else
+		printf("%i is negative\n", n);
+	return (0);
+}
