5d2ac966169dd582126029d2a651af6f8eea0e77
dumozitha <dumozitha@gmail.com>
2022-09-13 11:14:53 -0700

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,12 +1,14 @@
 #include"main.h"
 
 /**
- * print_alphabet_x10 - this will print 10x the alphabet
+ * print_alphabet_x10 - it will print 10x alphabet
  *
  */
+
 void print_alphabet_x10(void)
 {
 	int line, ch;
+
 	for (line = 0; line <= 9; ++line)
 	{
 		for (ch = 'a'; ch <= 'z'; ++ch)
