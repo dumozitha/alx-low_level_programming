cdd35e857cc0281534d97c08029e73790ba60c64
dumozitha <dumozitha@gmail.com>
2022-09-13 10:25:48 -0700

    task1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+ #include"main.h"
+
+/**
+ * no main
+ * Description: now printing in lower case
+ *
+ * no return
+ */
+void print_alphabet(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ++ch)
+		_putchar(ch);
+	_putchar('\n');
+}
