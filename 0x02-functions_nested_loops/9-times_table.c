4f784687b00b747ab43e3694db597f795607c1cf
dumozitha <dumozitha@gmail.com>
2022-09-14 05:37:13 -0700

    task9

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,7 +8,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <=9; ++num)
+	for (num = 0; num <= 9; ++num)
 	{
 		_putchar(48);
 		for (mult = 2; mult <= 9; ++mult)
@@ -22,7 +22,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48);
-			_putchar((prod % 10) +48);
+			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
 	}
