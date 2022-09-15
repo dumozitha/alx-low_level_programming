6af8682d36600fcd75cf94163370911f3d3c1126
dumozitha <dumozitha@gmail.com>
2022-09-14 17:33:39 -0700

    adv task1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -22,7 +22,6 @@ void print_times_table(int n)
 				_putchar(' ');
 
 				prod = num * mult;
-				
 				if (prod <= 9)
 					_putchar(' ');
 				if (prod <= 99)
@@ -31,7 +30,7 @@ void print_times_table(int n)
 				if (prod >= 100)
 				{
 					_putchar((prod / 100) + 48);
-					_putchar((prod /10) % 10 + 48);
+					_putchar((prod / 10) % 10 + 48);
 				}
 				else if (prod <= 99 && prod >= 10)
 					_putchar((prod / 10) + 48);
