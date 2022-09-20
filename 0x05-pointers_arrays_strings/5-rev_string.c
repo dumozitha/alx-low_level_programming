a65a67e253c22e1d19567ca4ebf167434f353e6a
dumozitha <dumozitha@gmail.com>
2022-09-20 11:49:21 -0700

    task5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,14 +11,17 @@ void rev_string(char *s)
 {
 	int l, i;
 	char ch;
-
+	
+	/*find string length without null char*/
 	for (l = 0; s[l] != '\0'; ++l)
 		;
 
+	/*swap the string by looping to half the string*/
 	for (l = 0; i < l / 2; ++i)
 	{
 		ch = s[i];
 		s[i] = s[l - 1 - i];
 		s[l - 1 - i] = ch;
 	}
+
 }
