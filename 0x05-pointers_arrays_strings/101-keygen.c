408fc69ed927231625903919c639dc56b4518428
dumozitha <dumozitha@gmail.com>
2022-09-20 15:30:02 -0700

    task101

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,27 @@
+#include"main.h"
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+
+/**
+ * main - creates random passwords
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	int pass, sum;
+
+	srand(time(NULL));
+	sum = 0;
+
+	while (sum <= 2645)
+	{
+		pass = (rand() % 128);
+		sum += pass;
+		printf("%c", pass);
+	}
+	printf("%c", 2772 - sum);
+
+	return (0);
+}
