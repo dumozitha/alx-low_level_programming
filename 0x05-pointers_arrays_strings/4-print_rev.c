22d5d61a4e11376ccb2a412a6196aed62722b20a
dumozitha <dumozitha@gmail.com>
2022-09-20 11:30:43 -0700

    task4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse
+ *
+ * @s: string input
+ *
+ * Return: Nothing
+ */
+void print_rev(char *s)
+{
+	int index;
+
+	for (index = 0; s[index] != '\0'; ++index)
+		;
+
+	for (--index; index >= 0; --index)
+		_putchar(s[index]);
+	_putchar('\n');
+}
