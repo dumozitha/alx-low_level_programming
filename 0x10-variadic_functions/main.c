04a526f0b621daf0e440b830fd9dcf60857971c1
dumozitha <dumozitha@gmail.com>
2022-10-14 13:12:48 -0700

    all tasks

---
+++ 0x10-variadic_functions/main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check code for ALX school
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	print_numbers(", ", 4, 0, 98, -1024, 402);
+	return (0);
+}
